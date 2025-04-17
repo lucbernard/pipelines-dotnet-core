63adb6f5e65099d1568e38e3df36ac1ea7234261
Luc Bernard <luc.bernard@gmail.com>
2025-04-17 10:41:30 -0400

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -16,7 +16,7 @@ namespace pipelines_dotnet_core
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container...
         public void ConfigureServices(IServiceCollection services)
         {
             _ = services.Configure<CookiePolicyOptions>(options =>
